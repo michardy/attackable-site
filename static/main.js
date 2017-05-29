24f021fdc2b67b5ecab03c24766d3888950379d5
Michael Hardy <mhardy17@bayschoolsf.org>
2017-05-29 14:01:46 -0700

    Initial console

---
+++ static/main.js
@@ -0,0 +1,30 @@
+var counter = 0;
+con = document.getElementById("console");
+
+function evaluate(cmd){
+	if (cmd === "help") {
+		document.getElementById("target" + counter).innerHTML += 'Type "reset users" to reset the user db<br/>Type "reset posts" to reset the post db<br/>Type "security &lt;on/off&gt;" to change security hardening<br/>';
+		ws.send(cmd);
+	}
+}
+
+function getcmd(e){
+	if (e.keyCode == 13){
+		document.getElementById("cmd" + counter).removeAttribute("contenteditable", false);
+		counter++;
+		init();
+		evaluate(document.getElementById("cmd" + (counter-1)).textContent);
+	}
+}
+
+function init (){
+	con.innerHTML += "<div id=\"target"+counter+"\"></div>";
+	con.innerHTML += "&gt;&gt;&gt;&nbsp;<span id=\"cmd"+counter+"\" onkeypress=\"getcmd(event);\" contenteditable=\"true\"></span><br/>";
+	document.getElementById("cmd" + counter).focus();
+}
+
+var ws = new WebSocket("wss://hijackingprevention.com/demo/console");
+ws.onmessage = function (evt) {
+   document.getElementById("target" + counter).innerHTML +=
+};
+init();
